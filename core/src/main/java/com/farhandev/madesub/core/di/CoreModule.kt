dbf21398ac534e76a76cfd014cd311de0974f369
GULTOM <firebasegultom@gmail.com>
2021-03-08 14:17:17 +0700

    certifiate pinner update

--- core/src/main/java/com/farhandev/madesub/core/di/CoreModule.kt
+++ core/src/main/java/com/farhandev/madesub/core/di/CoreModule.kt
@@ -10,6 +10,7 @@ import com.farhandev.madesub.core.domain.repository.XNewsRepository
 import com.farhandev.madesub.core.utils.AppExecutor
 import net.sqlcipher.database.SQLiteDatabase
 import net.sqlcipher.database.SupportFactory
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
@@ -34,10 +35,17 @@ val databaseModule = module {
 
 val networkModule = module {
     single {
+        val hostname = "newsapi.org"
+        val certificatePinner = CertificatePinner.Builder()
+                .add(hostname, "sha256/c5XTqkOxoXqc60M3HuT9fgmfeexiP2+Q8BD3+6abZYU=")
+                .add(hostname, "sha256/FEzVOUp4dF3gI0ZVPRJhFbSJVXR+uQmMH65xhs1glH4=")
+                .add(hostname, "sha256/Y9mvm0exBk1JoQ57f9Vm28jKo5lFm/woKcVxrYxu80o=")
+                .build()
         OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+                .certificatePinner(certificatePinner)
             .build()
     }
     single {
